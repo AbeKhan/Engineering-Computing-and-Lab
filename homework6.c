f2127e2bfb5d1559f36bd06cdafc70917446fd98
AbeKhan <abrahammkhan@gmail.com>
2025-03-11 16:15:55 -0500

    homework6 completed

--- homework6.c
+++ homework6.c
@@ -1,32 +1,68 @@
+/*
+        3/11/2025
+        Program Name: homework6.c
+        Name        : Abraham Khan
+
+        This program is designed to solve the homework
+        problem 6 for Engineering Computing Lab
+
+*/
+
 #include <stdio.h>
-#inlcude <math.h>
+#include <math.h>
+#include <stdbool.h>
 
 int main()
 {
-	bool exit  = false;
-	double a, b, c, Xin, Xfin;
-	do{
-		printf("*****************************************\n");
-		printf("*  Program to display table x and y on  *\n");
-		printf("*****************************************\n");
-		printf("Please enter a, b and c seperated by space:");
-		
-		scanf("%lf %lf %lf", &a, &b, &c);
-		
+	bool exit = false;
+	char check;
+	double a, b, c, Xin, Xfin, step, y;
+	printf("\n*****************************************\n");
+	printf("*  Program to display table x and y on  *\n");
+	printf("*****************************************\n");
+	printf("Please enter a, b and c seperated by space: ");
+
+	scanf("%lf %lf %lf", &a, &b, &c);
+	do
+	{
+
 		printf("\nPlease enter the iteration info\n");
-		prinft("x initial	: \n");
+		printf("x initial	: ");
 		scanf("%lf", &Xin);
-		prinft("x final 	: \n");
+		printf("x final		: ");
 		scanf("%lf", &Xfin);
-		
-		while (Xfin < Xin)
+
+		while (Xfin <= Xin)
 		{
-			prinft("x final has to be bigger than x initial\n");
-			prinft("Please re-enter x final: ");
+			printf("x final has to be bigger than x initial\n");
+			printf("Please re-enter x final: ");
 			scanf("%lf", &Xfin);
 		}
-	}
-	while(!exit)
-	
+
+		printf("step(min 0.1)	: ");
+		scanf("%lf", &step);
+
+		while (step < 0.1)
+		{
+			printf("step can't be less than 0.1\n");
+			printf("Pleaes re-enter step(min 0.1): ");
+			scanf("%lf", &step);
+		}
+
+		printf("  x 		y\n");
+
+		for (Xin; Xin < Xfin; Xin += step)
+		{
+			y = a * pow(Xin, 2) + b * Xin + c;
+			printf("%0.2lf		%0.2lf\n", Xin, y);
+		}
+
+		printf("Do you want another range (Y or N)? ");
+		scanf(" %c", &check);
+		if (check == 'N' || check == 'n')
+			exit = true;
+
+	} while (!exit);
+
 	return 0;
 }
