8384727fefc60845f565e4fbe0a0edfab602bb57
AbeKhan <abrahammkhan@gmail.com>
2025-04-10 12:33:14 -0500

    Create Writefile.c

---
+++ Writefile.c
@@ -0,0 +1,15 @@
+//Program to write file
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+	FILE *out;
+	
+	//Prepare the objecy file and cannect to thr external file
+	out = fopen(FILENAME, "w");
+	
+	
+	return 0;
+}
